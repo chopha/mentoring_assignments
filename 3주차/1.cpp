c1baa1e0d9e8aa326a822a28386c61c96e8ef675
parcon99 <parcon1999@gmail.com>
2021-02-18 13:33:35 +0900

    Create 1.cpp

---
+++ 3주차/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int array[5] = { 0 };
+	int total = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << i+1 << "번째 정수 입력:";
+		std::cin >> array[i];
+		total += array[i];
+	}
+	std::cout << "합계:" << total << std::endl;
+}
